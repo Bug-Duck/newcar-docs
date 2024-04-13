9bddb8e623a4d77c8fa2d850e7fdabe61055719a
Acbox <850625057@qq.com>
2024-04-13 21:59:57 +0800

    feat: examples

--- docs/examples/Demo1.vue
+++ docs/examples/Demo1.vue
@@ -5,28 +5,31 @@
 <script setup lang="ts">
 import { CarEngine, Scene, Widget, Circle, create, move, easeBounce, easeInCirc } from "newcar";
 import type { App } from "newcar";
+import { onMounted } from "vue";
 
 let app: App;
 
-new CarEngine()
-  .init("https://unpkg.com/canvaskit-wasm@latest/bin/canvaskit.wasm")
-  .then((engine) => {
-    app = engine.createApp(document.querySelector("#canvas1")!);
-    const root = new Widget().add(
-      new Circle(100, {
-        x: 800,
-        y: 450
-      })
-        .animate(create, 0, 30, {
-          by: easeInCirc
+onMounted(() => {
+  new CarEngine()
+    .init("https://unpkg.com/canvaskit-wasm@latest/bin/canvaskit.wasm")
+    .then((engine) => {
+      app = engine.createApp(document.querySelector("#canvas1")!);
+      const root = new Widget().add(
+        new Circle(100, {
+          x: 800,
+          y: 450
         })
-        .animate(move, 70, 100, {
-          from: [800, 450],
-          to: [800, 850],
-          by: easeBounce
-        })
-    );
-    const scene = new Scene(root);
-    app.checkout(scene);
-  });
+          .animate(create, 0, 30, {
+            by: easeInCirc
+          })
+          .animate(move, 70, 100, {
+            from: [800, 450],
+            to: [800, 850],
+            by: easeBounce
+          })
+      );
+      const scene = new Scene(root);
+      app.checkout(scene);
+    });
+});
 </script>
